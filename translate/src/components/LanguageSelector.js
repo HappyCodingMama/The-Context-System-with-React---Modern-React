3e14ae1094c249dbe965a848b7160eef47a2a96e
HappyCodingMama <happycodingmama@gmail.com>
2021-02-04 01:03:55 +0900

    build the translate app

---
+++ translate/src/components/LanguageSelector.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class LanguageSelector extends React.Component {
+  static contextType = LanguageContext;
+
+  render() {
+    return (
+      <div>
+        <h2>
+          Select a language:{' '}
+          <i
+            className='flag us'
+            onClick={() => this.context.onLanguageChange('english')}
+          ></i>
+          <i
+            className='flag nl'
+            onClick={() => this.context.onLanguageChange('dutch')}
+          ></i>
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default LanguageSelector;
